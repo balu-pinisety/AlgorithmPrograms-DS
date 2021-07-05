41bf63b3da87c611f0086d3559dfa628b1e2c343
balu-pinisety <manikantapandu225@gmail.com>
2021-07-05 05:52:22 +0530

    Added: File for Creation of BinarySearchWord Program

---
+++ AlgorithmPrograms/BinarySearchWord.java
@@ -0,0 +1,14 @@
+package com.AlgorithmPrograms;
+
+/**
+ * @author BALU
+ * Ability to search for the Word from Word List
+ */
+public class BinarySearchWord {
+
+	public static void main(String[] args) {
+		// Welcome Message
+		System.out.println("Program to search the word from Word List");
+	}
+
+}
